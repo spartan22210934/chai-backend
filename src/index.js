32edfb96f2bb89b852d95174059f96232508a881
Your Name <parbatkrushnav04@gmail.com>
2025-05-29 10:55:36 +0530

    mongo db connect

---
+++ src/index.js
@@ -0,0 +1,43 @@
+
+
+import connectDB from './db/index.js';
+
+import dotenv from "dotenv";
+dotenv.config(
+    {
+        path:'./env'
+    })
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+// import express from "express"
+
+// const app = express()
+
+// (async()=>{
+//     try {
+//    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//   app.on("error",()=>{
+//     console.log("ERRR:",error);
+//     throw error
+//   })
+//   app.listen(process.env.PORT,()=>{
+//     console.log(`app is listening on port${process.env.PORT}`)
+//   })
+
+//     } catch (error) {
+//         console.error("ERROR:",error)
+//         throw error
+        
+//     }
+// })()
